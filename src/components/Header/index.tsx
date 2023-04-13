072102180fd523d6df2397ad967c625ea474be51
Illia.Kaspiarovich <Illia.Kaspiarovich@easyfundraising.org.uk>
2023-04-13 23:36:17 +0200

    adds mui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import styles from './index.module.css'
+import Button from '@mui/material/Button';
 
 export const Header: React.FC = () => {
-  return <header className={styles.headerContainer}>Logo</header>
+  return (
+  <header className={styles.headerContainer}>
+    <Button variant="contained">Hello World</Button>
+  </header>
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline';
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import './index.css'
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
+    <CssBaseline enableColorScheme />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
