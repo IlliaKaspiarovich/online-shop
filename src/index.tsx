649ee961fe6526f6f7becc3b763855d5fd3a2a7e
Illia.Kaspiarovich <Illia.Kaspiarovich@easyfundraising.org.uk>
2023-04-13 15:36:09 +0200

    sets up code formatting

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router';
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,19 @@
-import { createBrowserRouter, Outlet } from 'react-router-dom';
+import { createBrowserRouter, Outlet } from "react-router-dom";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <div>home<Outlet/></div>,
-        children: [
-            {
-                path: "/lalala",
-                element: <div>lalala</div>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: (
+      <div>
+        home
+        <Outlet />
+      </div>
+    ),
+    children: [
+      {
+        path: "/lalala",
+        element: <div>lalala</div>,
+      },
+    ],
+  },
+]);
